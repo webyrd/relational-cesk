924f3f641856ce8303e0b07ca18374b5c9733df1
William Byrd <webyrd@cs.indiana.edu>
2013-03-26 21:56:43 -0600

    updated v-out examples

--- miniKanren-version/v-out-examples.scm
+++ miniKanren-version/v-out-examples.scm
@@ -3,47 +3,127 @@
 
 ;;; examples of the v-out technique
 
-;;; rembero
+(let ()
 
-(define rembero
-  (lambda (x ls out)
-    (conde
-      [(== '() ls) (== ls out)]
-      [(fresh (d)
-         (== `(,x . ,d) ls)
-         (rembero x d out))]
-      [(fresh (a d res)
-         (== `(,a . ,d) ls)
-         (=/= a x)
-         (== `(,a . ,res) out)
-         (rembero x d res))])))
+  (define rember
+    (lambda (x ls)
+      (cond
+        [(null? ls) '()]
+        [(eq? x (car ls)) (rember x (cdr ls))]
+        [else
+         (cons (car ls) (rember x (cdr ls)))])))
 
-(test "rembero-1"
-  (run* (q)
-    (rembero 'y '(x y z y w y y v) q))
-  '((x z w v)))
+  (printf "*** vanilla direct-style Scheme remember\n")
+  
+  (test "rember-1"
+    (rember 'y '(x y z y w y y v))
+    '(x z w v))
 
-(test "rembero-2"
-  (run* (q)
-    (rembero q '(x y z y w y y v) '(x z w v)))
-  '(y))
+  )
 
-(test "rembero-3"
-  (run 5 (q)
-    (rembero 'y q '(x z w v)))
-  '((x z w v)
-    (x z w v y)
-    (x z w y v)
-    (x z y w v)
-    (x y z w v)))
+(let ()
+  
+  (define rembero
+    (lambda (x ls out)
+      (conde
+        [(== '() ls) (== ls out)]
+        [(fresh (d)
+           (== `(,x . ,d) ls)
+           (rembero x d out))]
+        [(fresh (a d res)
+           (== `(,a . ,d) ls)
+           (=/= a x)
+           (== `(,a . ,res) out)
+           (rembero x d res))])))
 
-(test "rembero-4"
-  (run 5 (q)
-    (fresh (x ls out)
-      (rembero x ls out)
-      (== `(,x ,ls ,out) q)))
-  '((_.0 () ())
-    (_.0 (_.0) ())
-    ((_.0 (_.1) (_.1)) (=/= ((_.0 _.1))))
-    (_.0 (_.0 _.0) ())
-    ((_.0 (_.1 _.0) (_.1)) (=/= ((_.0 _.1))))))
+  (printf "*** vanilla direct-style remembero\n")
+  
+  (test "rembero-1"
+    (run* (q)
+      (rembero 'y '(x y z y w y y v) q))
+    '((x z w v)))
+
+  (test "rembero-2"
+    (run* (q)
+      (rembero q '(x y z y w y y v) '(x z w v)))
+    '(y))
+
+  (test "rembero-3"
+    (run 5 (q)
+      (rembero 'y q '(x z w v)))
+    '((x z w v)
+      (x z w v y)
+      (x z w y v)
+      (x z y w v)
+      (x y z w v)))
+
+  (test "rembero-4"
+    (run 5 (q)
+      (fresh (x ls out)
+        (rembero x ls out)
+        (== `(,x ,ls ,out) q)))
+    '((_.0 () ())
+      (_.0 (_.0) ())
+      ((_.0 (_.1) (_.1)) (=/= ((_.0 _.1))))
+      (_.0 (_.0 _.0) ())
+      ((_.0 (_.1 _.0) (_.1)) (=/= ((_.0 _.1))))))
+
+  )
+
+(let ()
+;;; Doesn't seem to be any point to v-out in direct-style--just
+;;; mirrors behavior of 'out'.
+  
+  (define rembero-aux
+    (lambda (x ls out v-out)
+      (conde
+        [(== '() ls) (== ls out) (== ls v-out)]
+        [(fresh (d)
+           (== `(,x . ,d) ls)
+           (rembero-aux x d out v-out))]
+        [(fresh (a d res v-out-res)
+           (== `(,a . ,d) ls)
+           (=/= a x)
+           (== `(,a . ,res) out)
+           (== `(,a . ,v-out-res) v-out)
+           (rembero-aux x d res v-out-res))])))
+
+  (define rembero
+    (lambda (x ls out)
+      (fresh (v-out)
+        (== out v-out)
+        (rembero-aux x ls out v-out))))
+
+  (printf "*** direct-style remembero with v-out\n")
+  
+  (test "rembero-1"
+    (run* (q)
+      (rembero 'y '(x y z y w y y v) q))
+    '((x z w v)))
+
+  (test "rembero-2"
+    (run* (q)
+      (rembero q '(x y z y w y y v) '(x z w v)))
+    '(y))
+
+  (test "rembero-3"
+    (run 5 (q)
+      (rembero 'y q '(x z w v)))
+    '((x z w v)
+      (x z w v y)
+      (x z w y v)
+      (x z y w v)
+      (x y z w v)))
+
+  (test "rembero-4"
+    (run 5 (q)
+      (fresh (x ls out)
+        (rembero x ls out)
+        (== `(,x ,ls ,out) q)))
+    '((_.0 () ())
+      (_.0 (_.0) ())
+      ((_.0 (_.1) (_.1)) (=/= ((_.0 _.1))))
+      (_.0 (_.0 _.0) ())
+      ((_.0 (_.1 _.0) (_.1)) (=/= ((_.0 _.1))))))
+
+  )
